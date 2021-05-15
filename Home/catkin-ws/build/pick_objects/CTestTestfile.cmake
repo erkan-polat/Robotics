ea3fe73ce5cb9f7d5a42ebcc6b87d83dad33abd5
Your Name <er_polat@hotmail.com>
2021-05-15 17:24:07 +0300

    c

---
+++ Home/catkin-ws/build/pick_objects/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/erkan/Udacity-Robotics-Software/Home/catkin-ws/src/pick_objects
+# Build directory: /home/erkan/Udacity-Robotics-Software/Home/catkin-ws/build/pick_objects
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
