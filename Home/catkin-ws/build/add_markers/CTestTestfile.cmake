c984637f4d5fe12aed4126127f406d8cf652c566
Your Name <er_polat@hotmail.com>
2021-05-15 16:25:13 +0300

    c

---
+++ Home/catkin-ws/build/add_markers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/erkan/Udacity-Robotics-Software/Home/catkin-ws/src/add_markers
+# Build directory: /home/erkan/Udacity-Robotics-Software/Home/catkin-ws/build/add_markers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Where-I-am/catkin-ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/erkan/Desktop/Where-I-am/catkin-ws/src
+# Build directory: /home/erkan/Desktop/Where-I-am/catkin-ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(teleop-twist-keyboard-master)
+subdirs(pgm_map_creator)
