4a1f28c3df6b088219670cabacc1ae0557d8c5bc
Your Name <er_polat@hotmail.com>
2021-05-15 16:56:10 +0300

    c

---
+++ Where-I-am/catkin-ws/build/pgm_map_creator/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/erkan/Desktop/Where-I-am/catkin-ws/src/pgm_map_creator
+# Build directory: /home/erkan/Desktop/Where-I-am/catkin-ws/build/pgm_map_creator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(msgs)
