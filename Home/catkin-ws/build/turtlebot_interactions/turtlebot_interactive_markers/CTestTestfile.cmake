1c57bb0c1b8d5225bc9d720e96d5b53da5ee1d20
Your Name <er_polat@hotmail.com>
2021-05-16 19:32:01 +0300

    c

--- Home/catkin-ws/build/turtlebot_interactions/turtlebot_interactive_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erkan/Udacity-Robotics-Software/Home/catkin-ws/src/turtlebot_interactions/turtlebot_interactive_markers
-# Build directory: /home/erkan/Udacity-Robotics-Software/Home/catkin-ws/build/turtlebot_interactions/turtlebot_interactive_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
